1f45cb1af54795726843eb06566a819bcc6f818a
Brett Bukowski <brett.bukowski@gmail.com>
2016-01-27 17:44:01 -0800

    Device Utilities
    - Moves device-related util functions into their own class
    - Adds #getModel, which returns the utsname `systemInfo.machine` name
    - Adds sanity unit tests

--- Example/Pods/Target Support Files/Tropicalytics/Tropicalytics-umbrella.h
+++ Example/Pods/Target Support Files/Tropicalytics/Tropicalytics-umbrella.h
@@ -6,11 +6,13 @@
 #import "TPLEvent.h"
 #import "TPLRequestManager.h"
 #import "TPLBatchDetails.h"
+#import "TPLDeviceInfo.h"
 #import "TPLFieldGroup.h"
 #import "TPLHeader.h"
 #import "Tropicalytics.h"
 #import "TPLUniqueIdentifier.h"
 #import "TPLAppUtilities.h"
+#import "TPLDeviceUtilities.h"
 #import "TPLUtilities.h"
 
 FOUNDATION_EXPORT double TropicalyticsVersionNumber;
---
+++ Example/Tests/TPLDeviceUtilitiesSpec.m
@@ -0,0 +1,76 @@
+//
+//  TPLDeviceUtilitiesSpec.m
+//  Tropicalytics
+//
+//  Created by Brett Bukowski on 1/27/16.
+//  Copyright © 2016 Tilt.com Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TPLDeviceUtilities.h"
+
+SpecBegin(TPLDeviceUtilities)
+
+describe(@"TPLDeviceUtilities", ^{
+    describe(@"getTimezone", ^{
+        it(@"returns the current timezone", ^{
+            NSString *result = [TPLDeviceUtilities getTimezone];
+            expect(result).to.match(@"^([a-zA-Z_/])*$");
+        });
+    });
+
+    describe(@"getLanguage", ^{
+        it(@"returns the current language", ^{
+            NSString *result = [TPLDeviceUtilities getLanguage];
+            // en or en-US depending on the setup.
+            expect(result).to.match(@"^([a-z]){2}(-([A-Z]){2})?$");
+        });
+    });
+    
+    describe(@"getCarrierName", ^{
+        it(@"returns nil when the device is not configured for a carrier", ^{
+            NSString *result = [TPLDeviceUtilities getCarrierName];
+            expect(result).to.beNil();
+        });
+    });
+    
+    describe(@"getNetwork", ^{
+        it(@"returns Unknown when the network can't be detected", ^{
+            NSString *result = [TPLDeviceUtilities getNetwork];
+            expect(result).to.equal(@"Unknown");
+        });
+    });
+    
+    describe(@"getOSVersion", ^{
+        it(@"returns OS version", ^{
+            NSString *result = [TPLDeviceUtilities getOSVersion];
+            expect(result).to.match(@"^([0-9]){1,2}\.([0-9])$");
+
+        });
+    });
+
+    describe(@"getName", ^{
+        it(@"returns device model name", ^{
+            NSString *result = [TPLDeviceUtilities getName];
+            // "iPhone"
+            expect(result).to.match(@"[a-zA-Z]");
+        });
+    });
+    
+    describe(@"getModel", ^{
+        it(@"returns device model name", ^{
+            NSString *result = [TPLDeviceUtilities getModel];
+            // "x86_64" on 64-bit Simulator
+            expect(result).to.match(@"[a-zA-Z]");
+        });
+    });
+    
+    describe(@"getUUID", ^{
+        it(@"returns InstallationUUIDKey", ^{
+            NSString *result = [TPLDeviceUtilities getUUID];
+            expect(result).to.match(@"^([a-z0-9]*)$");
+        });
+    });
+});
+
+SpecEnd
