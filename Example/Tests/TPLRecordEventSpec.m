dbbe124937ab7b218afb49d03ece42b174462051
Matt King <mattk@tilt.com>
2016-01-29 12:01:15 -0800

    Test check point

--- Example/Tests/TPLRecordEventSpec.m
+++ Example/Tests/TPLRecordEventSpec.m
@@ -13,85 +13,53 @@
 #import "TPLHeader.h"
 #import "Tropicalytics.h"
 #import "TPLAPIClient.h"
+#import "TPLRequestManager.h"
+#import <Expecta/Expecta.h>
 
-static NSString *const HerokuTestURL     = @"http://tropicalyticsresponseserver.herokuapp.com";
+static NSString *const HerokuTestURL      = @"http://tropicalyticsresponseserver.herokuapp.com";
+static NSUInteger const FlushRateConstant = 2;
 
-SpecBegin(TPLRecordEvent)
+SpecBegin(TPLRecordEventSpec)
 
-describe(@"RecordEvent", ^{
-    
-    describe(@"Successful API Request", ^{
-        it(@"Fails to send a TPLEvent to the API", ^{
-            waitUntil(^(DoneCallback done) {
-                TPLAPIClient *apiClient = [[TPLAPIClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://localhost"]];
-                //Sample JSON
-                NSDictionary *testEvent = @{@"type": @"Contribution",
-                                            @"client_tstamp": @1452291633,
-                                            @"ctx": @{
-                                                    @"campaign_info": @{
-                                                            @"guid": @"xxyyxxyxyxyxyxx",
-                                                            @"title": @"Chainsmokers All Day",
-                                                            @"creation_date": @"2016-01-01 00:00:00",
-                                                            @"expiration_date": @"2016-03-01 00:00:00",
-                                                            @"tilt_timestamp": @"2016-01-01 00:00:00",
-                                                            @"currency": @"USD",
-                                                            @"to_date_amt": @10,
-                                                            @"num_contributors": @1,
-                                                            @"target_amt": @10000,
-                                                            @"people_invited": @10,
-                                                            @"total_comments": @2,
-                                                            @"total_likes": @3,
-                                                            @"campaign_type": @"collect",
-                                                            @"campaign_security": @"private",
-                                                            @"target_timestamp": @"2016-03-01 00:00:00",
-                                                            @"created_platform": @"ios"
-                                                            }
-                                                    }
-                                            };
-                [apiClient postWithParameters:testEvent completion:^(NSDictionary *response, NSError *error) {
-                    XCTAssertNotNil(error, @"Error should be nil");
-                    done();
-                }];
-            });
-        });
+describe(@"TPLRecordEventSpec", ^{
+    __block TPLAPIClient *apiClient;
+    __block TPLRequestManager *requestManager;
+    it(@"can create an instance of TPLAPIClient for a Base URL", ^{
+        apiClient = [[TPLAPIClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://localhost"]];
+        requestManager = [[TPLRequestManager alloc] initWithAPIClient:apiClient];
+        expect(apiClient).toNot.beNil;
+    });
+        
+    it([NSString stringWithFormat:@"should have a flush rate of %lu",(unsigned long)FlushRateConstant] , ^{
+        requestManager.flushRate = 2;
+        expect(requestManager.flushRate).to.equal(FlushRateConstant);
     });
     
-    describe(@"Fail to send Record Event", ^{
-        it(@"Sends a TPLEvent to the API", ^{
-            waitUntil(^(DoneCallback done) {
-                TPLAPIClient *apiClient = [[TPLAPIClient alloc] initWithBaseURL:[NSURL URLWithString:HerokuTestURL]];
-                //Sample JSON
-                NSDictionary *testEvent = @{@"type": @"Contribution",
-                                            @"client_tstamp": @1452291633,
-                                            @"ctx": @{
-                                                    @"campaign_info": @{
-                                                            @"guid": @"xxyyxxyxyxyxyxx",
-                                                            @"title": @"Chainsmokers All Day",
-                                                            @"creation_date": @"2016-01-01 00:00:00",
-                                                            @"expiration_date": @"2016-03-01 00:00:00",
-                                                            @"tilt_timestamp": @"2016-01-01 00:00:00",
-                                                            @"currency": @"USD",
-                                                            @"to_date_amt": @10,
-                                                            @"num_contributors": @1,
-                                                            @"target_amt": @10000,
-                                                            @"people_invited": @10,
-                                                            @"total_comments": @2,
-                                                            @"total_likes": @3,
-                                                            @"campaign_type": @"collect",
-                                                            @"campaign_security": @"private",
-                                                            @"target_timestamp": @"2016-03-01 00:00:00",
-                                                            @"created_platform": @"ios"
-                                                            }
+    
+        
+        NSDictionary *testEvent = @{@"type": @"Contribution",
+                                    @"client_tstamp": @1452291633,
+                                    @"ctx": @{
+                                            @"campaign_info": @{
+                                                    @"guid": @"xxyyxxyxyxyxyxx",
+                                                    @"title": @"Chainsmokers All Day",
+                                                    @"creation_date": @"2016-01-01 00:00:00",
+                                                    @"expiration_date": @"2016-03-01 00:00:00",
+                                                    @"tilt_timestamp": @"2016-01-01 00:00:00",
+                                                    @"currency": @"USD",
+                                                    @"to_date_amt": @10,
+                                                    @"num_contributors": @1,
+                                                    @"target_amt": @10000,
+                                                    @"people_invited": @10,
+                                                    @"total_comments": @2,
+                                                    @"total_likes": @3,
+                                                    @"campaign_type": @"collect",
+                                                    @"campaign_security": @"private",
+                                                    @"target_timestamp": @"2016-03-01 00:00:00",
+                                                    @"created_platform": @"ios"
                                                     }
-                                            };
-                [apiClient postWithParameters:testEvent completion:^(NSDictionary *response, NSError *error) {
-                    XCTAssertNil(error, @"Error should not be nil");
-                    done();
-                }];
-            });
-            
-        });
-    });
+                                            }
+                                    };
 });
 
 SpecEnd
